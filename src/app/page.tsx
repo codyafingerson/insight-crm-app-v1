c9bc716e6dce143c0ee6b6df2c8a51ce8e4734c2
Cody Fingerson <codyfingerson0428@gmail.com>
2024-07-28 22:27:21 -0600

    Create the index page using the SignInForm component.
    Logic was also added in this component to verify if the user is signed in, and if they are, redirect them to the `/dashboard` page.
    
    A Spinner component was also constructed to display when the `isLoaded` (from the `useUser` hook) is not true.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+
+import SignInForm from "@/components/SignInForm";
+import { useRouter } from "next/navigation";
+import { useUser } from "@clerk/clerk-react";
+import { useEffect } from "react";
+import Spinner from "@/components/Spinner";
+
 export default function Home() {
+  const { isSignedIn, isLoaded } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      router.push("/dashboard");
+    }
+  }, [isLoaded, isSignedIn, router]);
+
+  if (!isLoaded) {
+    // Handle loading state however you like
+    return <Spinner />;
+  }
+
   return (
     <main>
-      <h1 className="text-xl">Insight CRM</h1>
+      <SignInForm />
     </main>
-  );
+  )
 }
