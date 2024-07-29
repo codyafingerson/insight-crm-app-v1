169832e42e39f8378e5f3c994b66264dcf854d1f
Cody Fingerson <codyfingerson0428@gmail.com>
2024-07-28 22:28:21 -0600

    Add insight branding colors for TailwindCSS styling.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      colors: {
+        'light-blue': '#158ff9',
+        'cyan-blue': '#00fefb',
+        'insight-blue': '#1593f9'
+      }
     },
   },
   plugins: [],
