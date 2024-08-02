dcca3cbd2f55c1ecd052954261dcf47730f93ed6
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-08-01 22:09:23 -0600

    Implement active class names

--- src/components/Sidebar/SidebarMenu.tsx
+++ src/components/Sidebar/SidebarMenu.tsx
@@ -7,6 +7,7 @@ import SidebarMenuItem from "./SidebarMenuItem";
 
 export default function SidebarMenu() {
     const pathname = usePathname();
+
     return (
         <ul className="space-y-2 font-medium">
             <SidebarMenuItem to="/dashboard" icon={faHouse} label="Dashboard" active={pathname === "/dashboard"} badge={1}/>
