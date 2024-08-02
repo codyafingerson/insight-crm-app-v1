7b4f1e648d5bd1513d8d0c75b6f76d5c5b9a89d4
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2024-08-01 22:08:45 -0600

    Add tailwindcss styling to CustomerList

--- src/components/CustomerList.tsx
+++ src/components/CustomerList.tsx
@@ -1,5 +1,7 @@
 'use client';
+
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 interface Customer {
   _id: string;
@@ -37,7 +39,7 @@ export default function CustomerList() {
 
   return (
     <div>
-      <h1>Customer List</h1>
+      {/* <h1>Customer List</h1>
       {customers.length > 0 ? (
         <ul>
           {customers.map((customer) => (
@@ -48,7 +50,72 @@ export default function CustomerList() {
         </ul>
       ) : (
         <p>No customers found.</p>
-      )}
+      )} */}
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                First Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Last Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Phone Number
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Company Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+              {/* <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                Apple MacBook Pro 17"
+              </th>
+              <td className="px-6 py-4">Silver</td>
+              <td className="px-6 py-4">Laptop</td>
+              <td className="px-6 py-4">$2999</td>
+              <td className="px-6 py-4">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Open
+                </a>
+              </td> */}
+
+              {customers.map((customer) => (
+                <tr key={customer._id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {customer.firstName}
+                  </th>
+                  <td className="px-6 py-4">{customer.lastName}</td>
+                  <td className="px-6 py-4">{customer.phoneNumber}</td>
+                  <td className="px-6 py-4">{customer.companyName}</td>
+                  <td className="px-6 py-4">
+                    <Link
+                      href="#"
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Open
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+
     </div>
   );
 }
